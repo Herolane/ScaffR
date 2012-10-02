d3cfd5f5860435ab67d6151fd270ea5f64ee2b03
Rod Johnson <rod@scaffr.com>
2012-10-02 02:18:35 -0600

    fixed mapping, moved current user to extension method

--- src/ScaffR.MVC.Shell/content/Controllers/BaseController.cs.pp
+++ src/ScaffR.MVC.Shell/content/Controllers/BaseController.cs.pp
@@ -1,33 +1,11 @@
 ﻿namespace $rootnamespace$.Controllers
 {
+    using System;
     using System.Web.Mvc;
-    using Core.Interfaces.Service;
     using Core.Model;
-    using System.Security.Claims;
 
     public abstract partial class BaseController : Controller
     {
-        private User _currentUser;
-        public User CurrentUser
-        {
-            get
-            {
-                if (_currentUser != null)
-                {
-                    return _currentUser;
-                }
-
-                var userService = DependencyResolver.Current.GetService<IUserService>();
-
-                _currentUser =  userService.GetByUsername(ClaimsPrincipal.Current.Identity.Name);
-                if (_currentUser == null && !string.IsNullOrEmpty(ClaimsPrincipal.Current.Identity.Name))
-                {
-                    var authenticationService = DependencyResolver.Current.GetService<IAuthenticationService>();
-                    authenticationService.SignOut();
-                }
-
-                return _currentUser;
-            }
-        }
+       
     }
 }
