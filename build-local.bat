de935f7eff599b1172f011b2a1c115b4bacaf63e
Rod Johnson <rod@scaffr.com>
2012-10-03 22:29:57 -0600

    updated version to 1.1.4

--- build-local.bat
+++ build-local.bat
@@ -2,12 +2,7 @@
 
 :: Set repository info
 set NUGET="nuget/nuget.exe"
-set key={your-api-key}
-set url={nuget-gallery-url}
 set path=c:\development\localnuget
-
-attrib -R %NUGET%
-cmd /c %NUGET% update -self
 
 IF not exist %path% mkdir %path%
 
@@ -16,7 +11,7 @@ for /R %%i in (*.nuspec) do (
  SET NUSPEC=%%i
  echo %%i
 
- %NUGET% pack %%i -o %path% -Version 1.1.3
+ %NUGET% pack %%i -o %path% -Version 1.1.4
 
 )
 
