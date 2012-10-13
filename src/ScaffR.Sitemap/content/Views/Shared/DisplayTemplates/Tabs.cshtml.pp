59737c972db7845817da6900f169e39dae85ad81
Rod Johnson <rod@scaffr.com>
2012-10-13 10:24:09 -0600

    fixed routing issue (Capitlization)

--- src/ScaffR.Sitemap/content/Views/Shared/DisplayTemplates/Tabs.cshtml.pp
+++ src/ScaffR.Sitemap/content/Views/Shared/DisplayTemplates/Tabs.cshtml.pp
@@ -4,8 +4,6 @@
     @foreach (var node in Model)
     {
         <li class="@Html.AddClass("active", node.IsCurrentNode || node.IsInCurrentPath && !node.IsRootNode)">
-            <a href="@Url.Action(node.Action, node.Controller)"><i class="@node.ImageUrl"></i>
-                @node.Title
-            </a></li>
+            <a href="@Url.Action(node.Action, node.Controller)">@node.Title</a></li>
     }
 </ul>
